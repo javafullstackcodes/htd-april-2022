1dd0d2104fb0ebb885354631031c451a523f5f64
asudheer09 <sudheer9.sr@gmail.com>
2022-04-19 09:51:29 +0530

    committing code for setters

--- java-operators/src/com/example/classes/ClassesandObjects.java
+++ java-operators/src/com/example/classes/ClassesandObjects.java
@@ -2,17 +2,28 @@ package com.example.classes;
 
 class Student{
 	
-	int sid;
-	String sname;
+	int sid; //0 
+	String sname; //null
+	String course; // null
 	
 	public Student() {
+		this(123,"sudheer","java"); // constructor call
 		System.out.println("0-args constructor");
+		
 	}
 	
-	public Student(int id,String name) {
+	public Student(int sid,String name) {
+		this.sid=sid;	//sid=123;
+		this.sname=name; //sname="sudheer"
+	}
+	
+	public Student(int id,String name,String c) {
 		sid=id;//sid=123;
 		sname=name; //sname="sudheer"
+		course=c;
 	}
+	
+	
 	
 }
 
@@ -21,12 +32,17 @@ public class ClassesandObjects {
 	public static void main(String[] args) {
 		
 		//className reference= new className();
+		Student s=  new Student();
+		
 		Student student=  new Student(10, "surendra");
+		
+		Student student1= new Student(11,"sudheer","java");
 		
 		//objectname.varaibleName
 		
-		System.out.println("student id is : "+student.sid); //0
-		System.out.println("student id is : "+student.sname); //null
+		System.out.println("student id is : "+s.sid); //0
+		System.out.println("student name is : "+s.sname); //null
+		System.out.println("student course is : "+s.course);
 
 	}
 
